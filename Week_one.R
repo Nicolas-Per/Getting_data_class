31e59c10075b496d19a0f6cda8435eff66002130
Nicolas Per <n.perez14@uniandes.edu.co>
2020-07-22 16:48:21 -0500

    Week one

---
+++ Week_one.R
@@ -0,0 +1,13 @@
+#Semana 1
+
+fileUrl->""  #Link de descarga
+download.file(Data, destfile = "", method = "curl") #Fución de descarga, destfile = destino.
+list.files("") 
+
+DateDownloaded<- date()
+DateDownloaded
+
+#Reading Excel data 
+
+install.packages("xlsx")
+library(xlsx)
