a1bb31e23ef0f7dbaf0dca7ca660e3f65929e50b
Nicolas Per <n.perez14@uniandes.edu.co>
2020-07-23 16:27:47 -0500

    continues

--- Week_one.R
+++ Week_one.R
@@ -11,3 +11,24 @@ DateDownloaded
 
 install.packages("xlsx")
 library(xlsx)
+
+#Read.table
+
+read.table(data, sep, header, quote="") #sep = formato (comas, tab...), quote = useful to skip the quotes of the file.  
+rea.csv #similar to read.table
+
+#data.table
+install.packages("data.table")
+library(data.table)
+tables()
+
+data<-read.table(file.choose(),h=T, sep = ",")
+data[list(mean(dia))]
+
+#Quiz 
+if(!file.exists("tax1.csv")){dir.create("tax1.csv")}
+file.exists("./tax1.csv")
+fileUrl<- "http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+download.file(fileUrl, destfile = "D/Uniandes/Data sciencce/Getting_data_class/tax1.csv", method="intern", mode="wb")
+data<-read.csv(file.choose(),h=T)
+subset<-subset(data, "VAL">=24)
